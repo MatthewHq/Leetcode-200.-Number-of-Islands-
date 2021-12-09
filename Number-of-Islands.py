e9ba7cfc8ba6fc87a9f303fc7fb7768fcf7203d7
MatthewHoque <hoque.matt@gmail.com>
2021-12-08 23:32:03 -0800

    islandCount mechanic

--- Number-of-Islands.py
+++ Number-of-Islands.py
@@ -6,12 +6,15 @@ class Solution:
     global visited
 
     def numIslands(self, grid: List[List[str]]) -> int:
+        islandCount=0
         m, n = len(grid), len(grid[0])
         visited = [[0 for x in range(n)] for y in range(m)]
         for i in range(n):
             for j in range(m):
                 if not visited[i][j] and grid[i][j]!=0:
+                    islandCount+=1
                     self.explore(grid, i, j)
+        return islandCount
 
     def explore(self, grid: List[List[str]], i, j):
         pass
