72a1e1b62c108ec515624561beb56d92cb6e23f8
MatthewHoque <hoque.matt@gmail.com>
2021-12-08 23:30:44 -0800

    barebones for loops that will explore each island

--- Number-of-Islands.py
+++ Number-of-Islands.py
@@ -1,15 +1,24 @@
 from typing import List
-#Python 3.8.3
+# Python 3.8.3
+
 
 class Solution:
+    global visited
+
     def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        visited = [[0 for x in range(n)] for y in range(m)]
+        for i in range(n):
+            for j in range(m):
+                if not visited[i][j] and grid[i][j]!=0:
+                    self.explore(grid, i, j)
+
+    def explore(self, grid: List[List[str]], i, j):
         pass
-    def explore(grid: List[List[str]],i,j):
-        pass
+        
 
 
-
-
-sol=Solution()
-grid=[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
-Solution.numIslands(sol,grid)
+sol = Solution()
+grid = [["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]]
+Solution.numIslands(sol, grid)
