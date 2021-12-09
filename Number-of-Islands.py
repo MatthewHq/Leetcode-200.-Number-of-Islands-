5dfc52dbc2b081e12da76faff206791eab5acbe9
MatthewHoque <hoque.matt@gmail.com>
2021-12-09 00:40:54 -0800

    complete, skipped a few commits because busy

--- Number-of-Islands.py
+++ Number-of-Islands.py
@@ -3,25 +3,42 @@ from typing import List
 
 
 class Solution:
-    global visited
-
     def numIslands(self, grid: List[List[str]]) -> int:
+        self.g = grid
         islandCount = 0
-        m, n = len(grid), len(grid[0])
-        visited = [[0 for x in range(n)] for y in range(m)]
-        for i in range(m):
-            for j in range(n):
-                print(i, j)
-                if not visited[i][j] and grid[i][j] != 0:
+        self.m, self.n = len(self.g), len(self.g[0])
+        self.visited = [[False for x in range(self.n)] for y in range(self.m)]
+        print(len(self.g), len(self.g[0]))
+        print(len(self.visited), len(self.visited[0]))
+        for i in range(self.m):
+            for j in range(self.n):
+                if not self.visited[i][j] and self.g[i][j] != '0':
                     islandCount += 1
-                    self.explore(grid, i, j)
+                    self.explore(i, j)
         return islandCount
 
-    def explore(self, grid: List[List[str]], i, j):
-        pass
+    def explore(self, i, j):
+        if i != 0:
+            if not self.visited[i-1][j] and self.g[i-1][j] != '0':
+                self.visited[i-1][j] = True
+                self.explore(i-1, j)
+        if j != 0:
+            if not self.visited[i][j-1] and self.g[i][j-1] != '0':
+                self.visited[i][j-1] = True
+                self.explore(i, j-1)
+        if i != self.m-1:
+            if not self.visited[i+1][j] and self.g[i+1][j] != '0':
+                self.visited[i+1][j] = True
+                self.explore(i+1, j)
+        if j != self.n-1:
+            if not self.visited[i][j+1] and self.g[i][j+1] != '0':
+                self.visited[i][j+1] = True
+                self.explore(i, j+1)
 
 
 sol = Solution()
-grid = [["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"],
-        ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]]
-Solution.numIslands(sol, grid)
+grid = [["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"]]
+print(Solution.numIslands(sol, grid))
