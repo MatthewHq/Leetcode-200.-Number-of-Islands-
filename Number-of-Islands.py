c2fafd083fd8a2597379cf81ee41a663a8fd6ed9
MatthewHoque <hoque.matt@gmail.com>
2021-12-09 00:03:52 -0800

    indexing fix

--- Number-of-Islands.py
+++ Number-of-Islands.py
@@ -6,19 +6,19 @@ class Solution:
     global visited
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        islandCount=0
+        islandCount = 0
         m, n = len(grid), len(grid[0])
         visited = [[0 for x in range(n)] for y in range(m)]
-        for i in range(n):
-            for j in range(m):
-                if not visited[i][j] and grid[i][j]!=0:
-                    islandCount+=1
+        for i in range(m):
+            for j in range(n):
+                print(i, j)
+                if not visited[i][j] and grid[i][j] != 0:
+                    islandCount += 1
                     self.explore(grid, i, j)
         return islandCount
 
     def explore(self, grid: List[List[str]], i, j):
         pass
-        
 
 
 sol = Solution()
