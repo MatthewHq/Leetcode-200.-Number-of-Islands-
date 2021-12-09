4cfddf9b8581beff48eb55cd9597294efb3f5a46
MatthewHoque <hoque.matt@gmail.com>
2021-12-08 22:47:25 -0800

    direction

--- Number-of-Islands.py
+++ Number-of-Islands.py
@@ -1,3 +1,15 @@
+from typing import List
+#Python 3.8.3
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
+        pass
+    def explore(grid: List[List[str]],i,j):
+        pass
+
+
+
+
+sol=Solution()
+grid=[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+Solution.numIslands(sol,grid)
